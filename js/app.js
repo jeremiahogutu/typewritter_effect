491e27b9edd61b6a6864ef5a27fbb80ae1c4b35e
Jamie O <jeremiahogutu@protonmail.com>
2017-07-07 21:21:09 -0400

    typed js test
    typewritter effect test

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(".type").typed({
+        strings: [
+            "Hello World!",
+            "My Name is Jeremiah Ogutu",
+            "I am a web developer/designer"
+        ],
+        typeSpeed: 70,
+        backDelay: 200,
+        // loop: true
+    });
+});
+
